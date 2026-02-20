ac8e2592c98e05645559841f4306119527dd1f4a
YehiaHany <hanyyehia233@gmail.com>
2026-02-20 16:51:03 +0200

    feature(ui):merged on boarding files with our project files and created localization for the used text

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -2,4 +2,6 @@ class AppRoutes{
   static const String updateProfileScreen = "update_profile_screen";
   static const String loginScreen='LoginScreen';
   static const String registerScreen = "register_screen";
+  static const String onBoardingScreen = "on_boarding_screen";
+
 }
