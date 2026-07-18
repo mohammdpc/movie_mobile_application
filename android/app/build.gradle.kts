b2f1c69ef8c974d37043b2dab5cc7613ca35d68a
mohammd <mohammdpc2018@gmail.com>
2026-07-18 21:26:56 +0300

    final with screenshots

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.example.movie"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
 //    ndkVersion = "27.0.12077973"
     
     // my settings
-    ndkVersion = "29.0.14206865"
-    //buildToolsVersion = "36.0.0"
+    ndkVersion = "28.2.13676358"
+    buildToolsVersion = "36.0.0"
     
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -31,7 +31,7 @@ android {
         applicationId = "com.example.movie"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
